4dbe77cc939ae08655d9438960496e92df29ab71
IWTChenge <43315926+IWTChenge@users.noreply.github.com>
2023-09-11 13:57:22 +0800

    Delete 执行远程shell脚本.sh

--- 执行远程shell脚本.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for a in {20..79}
-do
-echo "=================== On 192.168.202.$a ========================";
-ssh root@192.168.202.$a "source /etc/profile;cd /root/NLP/tcpT/; sh sh_singleClient_startUp.sh"
-done
-
